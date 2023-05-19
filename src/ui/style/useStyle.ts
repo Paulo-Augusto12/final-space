e161b35a96ef316571688fdbf57790c055017e50
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-19 12:34:17 -0300

    refactor: changed the way theme object is going to be used

--- src/ui/style/useStyle.ts
+++ src/ui/style/useStyle.ts
@@ -7,20 +7,17 @@ export function useStyle() {
     purpleLight: "#0056A6",
     fontColor: "#FFFFFF",
   };
-  const sizes = [
-    {
-      rg: "1.5rem",
-      md: "1.875rem",
-      sm: "0.9375rem",
-    },
-  ];
-  const weights = [
-    {
-      rg: 400,
-      bd: 700,
-    },
-  ];
-  const fonts = [{}];
+  const sizes = {
+    rg: "1.5rem",
+    md: "1.875rem",
+    sm: "0.9375rem",
+  };
+
+  const weights = {
+    rg: 400,
+    bd: 700,
+  };
+  const fonts = {};
 
   return {
     colors,
