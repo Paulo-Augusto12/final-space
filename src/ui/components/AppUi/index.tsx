84659c9340bc1a3f522042c7e70f852391b8f8a9
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-22 10:01:39 -0300

    chore: exporting in development component

--- src/ui/components/AppUi/index.tsx
+++ src/ui/components/AppUi/index.tsx
@@ -1,4 +1,5 @@
 export { CharacterCard } from "@/CharacterCard";
 export {Header} from '@/Header'
 export { AppLayout } from '@/AppLayout'
+export { InDevelopment } from '@/InDevelopment'
 export { Button, Typography, Layout, Space, Spin } from "antd";
