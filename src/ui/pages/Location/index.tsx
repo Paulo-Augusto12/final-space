c14dc34cf8dbb8c1ef1e4942a8ad88126fcc6dd3
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-22 09:28:46 -0300

    chore: added style to another page to see its behavior

--- src/ui/pages/Location/index.tsx
+++ src/ui/pages/Location/index.tsx
@@ -1,8 +1,24 @@
+import { AppLayout } from "@/AppLayout";
+import { useStyle } from "../../style/useStyle";
+
 export function Location() {
+  const { colors } = useStyle();
   return (
-    <div>
-      <h1>Hello world</h1>
-      <h3>Quotes</h3>
-    </div>
+    <AppLayout
+      contentStyle={{
+        backgroundColor: colors.background,
+        display: "flex",
+        gridTemplateColumns: "repeat(3, 2fr)",
+        alignItems: "center",
+        justifyItems: "center",
+        padding: "3rem",
+        gap: "5.625rem",
+      }}
+    >
+      <div>
+        <h1>Hello world</h1>
+        <h3>Quotes</h3>
+      </div>
+    </AppLayout>
   );
 }
