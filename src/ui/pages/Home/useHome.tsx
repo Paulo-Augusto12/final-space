16000ac28a028e05d1ceffbd8eba3abd22082d21
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-22 08:40:17 -0300

    chore: created an array of elements that will be renderen in the header

--- src/ui/pages/Home/useHome.tsx
+++ src/ui/pages/Home/useHome.tsx
@@ -1,7 +1,17 @@
 import { useState, useEffect } from "react";
 import { SimpleCharacter } from "../../../domain/useCases/models/SimpleCharacter";
 
+// Icons
+
+import { FinnTheHuman, MapPin, Quotes } from "@phosphor-icons/react";
+//
+
+// Dependencies
+
 import { getcharsuc } from "../../../di/index";
+
+//
+
 export function useHome() {
   const [charl, setCharl] = useState<SimpleCharacter[]>([]);
 
@@ -10,15 +20,35 @@ export function useHome() {
 
     setCharl(data);
   }
-  
-  
+
+  const headerItems = [
+    {
+      name: "Characters",
+      icon: <FinnTheHuman size={32} />,
+      route: "/",
+    },
+    {
+      name: "Locations",
+      icon: <MapPin size={32} />,
+      route: "/locations",
+    },
+    {
+      name: "Get a quote",
+      icon: <Quotes size={32} />,
+      route: "/quotes",
+    },
+  ];
+
   useEffect(() => {
-      handleGetChars()
-  },[])
+    handleGetChars();
+  }, []);
 
   return {
     states: {
       charl,
+    },
+    elements: {
+      headerItems,
     },
   };
 }
