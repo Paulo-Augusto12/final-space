d7adc8891b3e8f52cb83df3203469af9cb9a20d2
Paulo Augusto <paulo-augusto12@outlook.com>
2023-05-22 10:28:51 -0300

    feat: developed footer component

--- src/ui/components/Footer/index.tsx
+++ src/ui/components/Footer/index.tsx
@@ -1,7 +1,76 @@
+import React from "react";
+import { Typography } from "@/AppUi";
+
+// Icons
+
+import {
+  TreeStructure,
+  GithubLogo,
+  TelevisionSimple,
+} from "@phosphor-icons/react";
+
+//
+
+import { useStyle } from "../../style/useStyle";
+
 export function Footer() {
+  const { colors } = useStyle();
+  const footerItems = [
+    {
+      name: "Api Used",
+      icon: <TreeStructure size={32} />,
+      link: "https://finalspaceapi.com/docs/",
+    },
+    {
+      name: "Visit my github",
+      icon: <GithubLogo size={32} />,
+      link: "https://github.com/Paulo-Augusto12",
+    },
+    {
+      name: "About the show",
+      icon: <TelevisionSimple size={32} />,
+      link: "https://en.wikipedia.org/wiki/Final_Space",
+    },
+  ];
   return (
-    <div>
-      <h1></h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        width: "100%",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          gap: "75px",
+        }}
+      >
+        {footerItems.map(({ icon, name, link }, index) => (
+          <a href={link} key={index}>
+            <Typography.Title
+              style={{
+                color: colors.fontColor,
+                display: "flex",
+                alignItems: "center",
+                gap: "7px",
+              }}
+              level={2}
+            >
+              {name} {icon}
+            </Typography.Title>
+          </a>
+        ))}
+      </div>
+      <img
+        src="https://finalspaceapi.com/img/logo.png"
+        width={"216px"}
+        height={"132px"}
+        style={{ opacity: "50%" }}
+      />
     </div>
   );
 }
